251b67a25eaf43d1482acd3fd49bd16d50fdfaeb
Quianna <quiannarolston@gmail.com>
2022-10-30 14:44:26 -0700

    step 2 pt 1 complete

--- fintech_finder.py
+++ fintech_finder.py
@@ -239,11 +239,11 @@ st.sidebar.markdown("## Total Wage in Ether")
 # Calculate total `wage` for the candidate by multiplying the candidate’s hourly
 # rate from the candidate database (`candidate_database[person][3]`) by the
 # value of the `hours` variable
-# YOUR CODE HERE
+wage = hourly_rate * hours
 
 # @TODO
 # Write the `wage` calculation to the Streamlit sidebar
-# YOUR CODE HERE
+st.sidebar.write(wage)
 
 ##########################################
 # Step 2 - Part 2:
